f2df439f6a7d52528c5abfb7e9576d2f607b1c2a
Guilherme Moretti <160153078+gmorettirib@users.noreply.github.com>
2025-05-14 12:10:34 -0300

    Add files via upload

---
+++ aula10/conta.class.php
@@ -0,0 +1,31 @@
+<?php
+    class Conta{
+        public $Nome;
+        public $Cpf;
+        private $Saldo;
+
+        public function Sacar($valor){
+            if($valor > $this->Saldo){
+                $resultado =  "Saldo Insuficiente";
+                return $resultado;
+            }else{
+                $this->Saldo = $this->Saldo - $valor;
+                $resultado =  "Saldo atualizado para R$ ". $this->Saldo;
+                return $resultado;
+            }
+
+
+        }
+        public function Depositar($valor){
+            $this->Saldo = $this->Saldo + $valor;
+            $resultado =  "Saldo atualizado para R$ ". $this->Saldo;
+            return $resultado;
+        }
+        public function ConsultarSaldo(){
+            echo "NOME: " . $this->Nome . "<br>"; 
+            echo "CPF: " . $this->Cpf . "<br>";
+            echo "Saldo Atual R$ " . $this->Saldo . "<br>";
+        }
+    }
+
+?>
